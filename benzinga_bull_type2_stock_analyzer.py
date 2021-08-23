d9b623ceaf9b3e5eb658322ca988098424c19207
Anand Nair <anand.nair@personalis.com>
2021-08-22 22:31:23 -0700

    Changing the logic to get gainers for benzinga.

--- benzinga_bull_type2_stock_analyzer.py
+++ benzinga_bull_type2_stock_analyzer.py
@@ -12,23 +12,49 @@ import pandas as pd
 import time
 from bull_flag_pattern import BullFlagPattern
 from benzinga_screener import BenzingaScreener
+from bull_flag_verifier import BullFlagVerifier
+from bull_flag_verifier import Transaction
+import os
+from datetime import date
 
 def main(assHand=None):
     benzingaScreener = BenzingaScreener();
-    while True:
-        stocks = benzingaScreener.get_gainers();
-        loserStocks = benzingaScreener.get_losers();
-        bullFlatPattern = BullFlagPattern();
-        for stock in stocks:
-            print("Working on stock", stock)
-            bullFlatPattern.checkType2BullFlagPattern(stock, interval='5Min',limit=78)
+    realtime = 0
+    limit = 10
+    if (realtime == 0):
+        limit = 78
 
-        for stock in loserStocks:
-            print("Working on stock", stock)
-            bullFlatPattern.checkType2BullFlagPattern(stock, interval='5Min',limit=78)
+    stock_alert_filename = "predictions/benzinga/"+"stock_alert_bull_type_2_" + str(date.today()) + ".csv"
+    stoploss_alert_filename = "predictions/benzinga/"+"stoploss_alert_bull_type_2_" + str(date.today()) + ".csv"
+    target_achieved_filename = "predictions/benzinga/"+"target_achieved_bull_type_2_" + str(date.today()) + ".csv"
+    bull_flag_2_alert_file = open(stock_alert_filename, "a+")
+    stoploss_alert_file = open(stoploss_alert_filename, "a+")
+    target_achieved_alert_file = open(target_achieved_filename, "a+")
+
+    if (os.stat(stock_alert_filename).st_size == 0):
+        bull_flag_2_alert_file.write("Stock, Type, Time, Stoploss")
+        bull_flag_2_alert_file.write("\n")
+    if (os.stat(stoploss_alert_filename).st_size == 0):
+        stoploss_alert_file.write("Stock, Type, Time, Stoploss")
+        stoploss_alert_file.write("\n")
+    if (os.stat(target_achieved_filename).st_size == 0):
+        target_achieved_alert_file.write("Stock, Type, Time, Stoploss")
+        target_achieved_alert_file.write("\n")
+
+    while True:
+        stocks = benzingaScreener.get_gainers_for_session();
+        bullFlatPattern = BullFlagPattern();
+        transaction = Transaction(0, 0);
+        for stock in stocks:
+            # print("Working on stock", stock)
+            bullFlagInformation = bullFlatPattern.checkType2BullFlagPattern(stock, bull_flag_2_alert_file,
+                                                                            interval='5Min', limit=limit)
+            if (realtime == 0):
+                bull_flag_verifier = BullFlagVerifier()
+                bull_flag_verifier.verifyBullFlag(bullFlagInformation, transaction)
         print("________________________________________________________________________________________________")
+
         print("Sleeping")
         time.sleep(120)
-
 if __name__ == '__main__':
     main()
--- benzinga_screener.py
+++ benzinga_screener.py
@@ -4,7 +4,7 @@ import pandas as pd
 class BenzingaScreener:
 
     def get_gainers(self):
-        response = requests.get("https://data-api-pro.benzinga.com/rest/movers?apikey=aH0FkLCohY5yxK6OEaJ28Zpv51Ze1GyY&maxResults=100&session=REGULAR&screenerQuery=marketCap_gt_300000000%3BpreviousClose_gt_5&gainers=true")
+        response = requests.get("https://data-api-pro.benzinga.com/rest/movers?apikey=aH0FkLCohY5yxK6OEaJ28Zpv51Ze1GyY&from=-60m&maxResults=100&session=REGULAR&screenerQuery=marketCap_gt_300000000&gainers=true")
         result = response.json()
         gainers = result["result"]["gainers"]
         gainersSortedByVol = pd.json_normalize(gainers).sort_values(by=['volume'], ascending=False)
@@ -14,10 +14,21 @@ class BenzingaScreener:
 
     def get_losers(self):
         response = requests.get(
-                "https://data-api-pro.benzinga.com/rest/movers?apikey=aH0FkLCohY5yxK6OEaJ28Zpv51Ze1GyY&maxResults=100&session=REGULAR&screenerQuery=marketCap_gt_300000000%3BpreviousClose_gt_5&losers=true")
+                "https://data-api-pro.benzinga.com/rest/movers?apikey=aH0FkLCohY5yxK6OEaJ28Zpv51Ze1GyY&from=-60m&maxResults=100&session=REGULAR&screenerQuery=marketCap_gt_300000000&gainers=true")
         result = response.json()
         losers = result["result"]["losers"]
         losersSortedByVol = pd.json_normalize(losers).sort_values(by=['volume'], ascending=False)
         print(losersSortedByVol)
         loserSymbols = losersSortedByVol['symbol'].values.tolist()
         return loserSymbols;
+
+    def get_gainers_for_session(self):
+        response = requests.get("https://data-api-pro.benzinga.com/rest/movers?apikey=aH0FkLCohY5yxK6OEaJ28Zpv51Ze1GyY&maxResults=100&session=REGULAR&screenerQuery=marketCap_gt_300000000%3BpreviousClose_gt_5&gainers=true")
+        result = response.json()
+        gainers = result["result"]["gainers"]
+        gainersSortedByVol = pd.json_normalize(gainers).sort_values(by=['volume'], ascending=False)
+        print(gainersSortedByVol)
+        gainerSymbols = gainersSortedByVol['symbol'].values.tolist()
+        return gainerSymbols;
+
+
