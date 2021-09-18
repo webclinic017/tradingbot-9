e15ee8ef47d0b382c28722d6edc6b88a77486b26
Anand Nair <anand.nair@personalis.com>
2021-09-18 12:50:27 -0700

    Refactoring.

--- bull_stock_analyzer.py
+++ bull_stock_analyzer.py
@@ -5,25 +5,28 @@ from benzinga_screener import BenzingaScreener
 from bull_flag_pattern import BullFlagPattern
 from bull_flag_verifier import BullFlagVerifier
 from bull_flag_verifier import Transaction
+from finviz_screener import FinvizScreener
 from traderlib import *
 
 
 def main():
     benzinga_screener = BenzingaScreener();
-    realtime = 1
+    finvizScreener = FinvizScreener();
+
+    realtime = 0
     limit = 10
     interval = '5Min';
     if realtime == 0:
         limit = 78
 
-    stock_alert_filename = "predictions/benzinga/" + "stock_alert_bull_" + str(date.today()) + ".csv"
+    stock_alert_filename = "predictions/" + "alerts/stock_alert_bull_" + str(date.today()) + ".csv"
     trade_executions_filename = "predictions/trade_executions_" + str(date.today()) + ".csv"
-    benzinga_stock_alert_file = open(stock_alert_filename, "a+")
+    stock_alert_file = open(stock_alert_filename, "a+")
     trade_executions_file = open(trade_executions_filename, "a+")
 
     if os.stat(stock_alert_filename).st_size == 0:
-        benzinga_stock_alert_file.write("Stock, Type, Time, Stoploss")
-        benzinga_stock_alert_file.write("\n")
+        stock_alert_file.write("Stock, Type, Time, Stoploss")
+        stock_alert_file.write("\n")
     if os.stat(trade_executions_filename).st_size == 0:
         trade_executions_file.write("Stock, Type, Time, Stoploss, Entry Price, R, Target Price, Prediction Type")
         trade_executions_file.write("\n")
@@ -34,22 +37,29 @@ def main():
     stock_info = {}
     while True:
         # Get all the gainers for the session
-        stocks = benzinga_screener.get_gainers_for_session();
+        benzinga_stocks = benzinga_screener.get_gainers_for_session();
+        stocks = benzinga_stocks;
+        try:
+            finviz_stocks = finvizScreener.positive_movers_with_beta_over_2()
+            stocks = stocks + finviz_stocks;
+        except Exception as e:
+            print("Exception fetching finviz stocks", e)
+        print("Total Stocks", len(stocks))
+
+        stocks = list(dict.fromkeys(stocks))
+        print("After removing duplicates Total Stocks", len(stocks))
+
         bull_flat_pattern = BullFlagPattern();
         transaction = Transaction(0, 0);
         for stock in stocks:
-
-            # print("Working on stock", stock)
-            # Fetch the recent candle information for the given stock
             candle_infos = api.get_barset(stock, interval, limit).df
             if realtime == 1:
                 candle_infos = candle_infos[0:-1]
                 
-            # Apply Bull flag 1 pattern match. If found, enter the values to benzinga_stock_alert_file
-            bull_flag1 = bull_flat_pattern.checkType1BullFlagPattern(stock, candle_infos, benzinga_stock_alert_file,
+            bull_flag1 = bull_flat_pattern.checkType1BullFlagPattern(stock, candle_infos, stock_alert_file,
                                                                      stock_info, limit)
-            # Apply Bull flag 2 pattern match. If found, enter the values to benzinga_stock_alert_file
-            bull_flag2 = bull_flat_pattern.checkType2BullFlagPattern(stock, candle_infos, benzinga_stock_alert_file,
+
+            bull_flag2 = bull_flat_pattern.checkType2BullFlagPattern(stock, candle_infos, stock_alert_file,
                                                                      stock_info, limit)
 
             if realtime == 0:
